0e21c4637d28b21627f0f3f04166b5f6d6024ebb
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-11-26 11:43:14 -0700

    Update lib.rs, add File
    Update main function for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use serde::{Deserialize, Serialize};
 
 mod client;
+mod executor;
 mod http;
+
 pub use client::Client;
+pub use executor::{ExecutionResult, Executor, ExecutorResponse};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Language {
@@ -19,5 +22,47 @@ impl Language {
             version,
             aliases,
         }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct File {
+    pub name: String,
+    pub content: String,
+    pub encoding: String,
+}
+
+impl Default for File {
+    fn default() -> Self {
+        Self {
+            name: "".to_string(),
+            content: "".to_string(),
+            encoding: "utf8".to_string(),
+        }
+    }
+}
+
+impl File {
+    pub fn new(name: &str, content: &str, encoding: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            content: content.to_string(),
+            encoding: encoding.to_string(),
+        }
+    }
+
+    pub fn set_content(mut self, content: &str) -> Self {
+        self.content = content.to_string();
+        self
+    }
+
+    pub fn set_name(mut self, name: &str) -> Self {
+        self.name = name.to_string();
+        self
+    }
+
+    pub fn set_encoding(mut self, encoding: &str) -> Self {
+        self.encoding = encoding.to_string();
+        self
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-use piston_rs::Client;
+use piston_rs::{Client, Executor, File};
 
 // Here for testing purposes
 
 #[tokio::main]
 async fn main() {
-    let mut client = Client::default();
-    match client.fetch_languages().await {
-        Ok(_) => (),
-        Err(e) => println!("{}", e),
-    }
+    // Create a new mutable Client
+    let client = Client::default();
 
-    println!("{:?}", client);
+    // // Fetch languages from Piston. Note: The languages are added onto the
+    // // Client, and a reference to them is returned.
+    // // If there was an error, print it and return from the main function.
+    // if let Err(e) = client.fetch_languages().await {
+    //     println!("{}", e);
+    //     return;
+    // }
+
+    // Create a new File to send to Piston. This will contain our source code.
+    let file = File::default().set_name("test.py").set_content(
+        "import random
+print(random.randint(1, 10))
+print('Hello from python!')",
+    );
+
+    // Create a new executor, this represents the language, other metadata
+    // about the code we are sending to Piston.
+    let executor = Executor::default()
+        .set_language("python")
+        .set_version("3.10")
+        .add_file(file);
+
+    // Send the response to Piston, and store the result.
+    let result = client.execute(&executor).await;
+
+    if let Ok(data) = result {
+        println!("{}", data.run.output);
+    } else {
+        println!("{}", result.unwrap_err());
+    }
 }
