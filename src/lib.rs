eba19402a0661498522a22d1ee8b9fe82825b0cb
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-12-30 07:02:00 -0700

    Impl LoadError

--- src/lib.rs
+++ src/lib.rs
@@ -73,6 +73,45 @@ pub struct Runtime {
     pub aliases: Vec<String>,
 }
 
+/// The result from attempting to load a [`File`].
+type LoadResult<T> = Result<T, LoadError>;
+
+/// The error that is returned when loading from a [`File`] on disk
+/// fails for any reason.
+#[derive(Debug, Clone)]
+pub struct LoadError {
+    /// The details of this error.
+    pub details: String,
+}
+
+impl LoadError {
+    /// Generates a new [`LoadError`].
+    ///
+    /// # Arguments
+    /// - `details` - The details of the error.
+    ///
+    /// # Returns
+    /// - [`LoadError`] - The new error.
+    ///
+    /// # Examples
+    /// ```
+    /// let e = piston_rs::LoadError::new("err");
+    ///
+    /// assert_eq!(e.details, "err".to_string())
+    /// ```
+    pub fn new(details: &str) -> Self {
+        Self {
+            details: details.into(),
+        }
+    }
+}
+
+impl std::fmt::Display for LoadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
+
 /// A file that contains source code to be executed.
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct File {
@@ -150,42 +189,37 @@ impl File {
     ///
     /// # Example
     /// ```
-    /// let file = piston_rs::File::load_from("src/lib.rs");
+    /// let file = piston_rs::File::load_from("src/lib.rs").unwrap();
     ///
     /// assert!(file.content.contains("pub fn load_from"));
     /// assert_eq!(file.name, "lib.rs".to_string());
     /// assert_eq!(file.encoding, "utf8".to_string());
     /// ```
-    pub fn load_from(path: &str) -> Self {
+    pub fn load_from(path: &str) -> LoadResult<Self> {
         let path = PathBuf::from(path);
 
         if !path.is_file() {
-            println!("File does not exist, or is a directory -- using defaults");
-            return File::default();
+            return Err(LoadError::new("File does not exist, or is a directory"));
         }
 
         let name = match path.file_name() {
-            Some(n) => n.to_str().unwrap_or("Invalid file name"),
+            Some(n) => n.to_string_lossy(),
             None => {
-                println!("Invalid file name");
-                ""
+                return Err(LoadError::new("Unable to parse file name"));
             }
         };
 
-        Self {
+        Ok(Self {
             name: name.to_string(),
-            content: File::load_contents(&path),
+            content: File::load_contents(&path)?,
             encoding: String::from("utf8"),
-        }
+        })
     }
 
-    fn load_contents(path: &Path) -> String {
-        match fs::read_to_string(&path) {
-            Ok(c) => c,
-            Err(e) => {
-                println!("{}", e);
-                String::new()
-            }
+    fn load_contents(path: &Path) -> LoadResult<String> {
+        match fs::read_to_string(path) {
+            Ok(content) => Ok(content),
+            Err(e) => Err(LoadError::new(&e.to_string())),
         }
     }
 
@@ -204,6 +238,7 @@ impl File {
     ///
     /// assert_eq!(file.content, "print(\"Hello, world!\")".to_string());
     /// ```
+    #[must_use]
     pub fn set_content(mut self, content: &str) -> Self {
         self.content = content.to_string();
         self
@@ -223,12 +258,13 @@ impl File {
     /// let file = piston_rs::File::default()
     ///     .load_contents_from("src/lib.rs");
     ///
-    /// assert!(file.content.contains("pub fn load_contents_from"));
+    /// assert!(file.is_ok());
+    /// assert!(file.unwrap().content.contains("pub fn load_contents_from"));
     /// ```
-    pub fn load_contents_from(mut self, path: &str) -> Self {
+    pub fn load_content_from(mut self, path: &str) -> LoadResult<Self> {
         let path = PathBuf::from(path);
-        self.content = File::load_contents(&path);
-        self
+        self.content = File::load_contents(&path)?;
+        Ok(self)
     }
 
     /// Sets the name of the file.
@@ -246,6 +282,7 @@ impl File {
     ///
     /// assert_eq!(file.name, "__main__.py".to_string());
     /// ```
+    #[must_use]
     pub fn set_name(mut self, name: &str) -> Self {
         self.name = name.to_string();
         self
@@ -267,6 +304,7 @@ impl File {
     ///
     /// assert_eq!(file.encoding, "hex".to_string());
     /// ```
+    #[must_use]
     pub fn set_encoding(mut self, encoding: &str) -> Self {
         self.encoding = encoding.to_string();
         self
@@ -276,12 +314,13 @@ impl File {
 #[cfg(test)]
 mod test_file_private {
     use super::File;
+    use super::Runtime;
     use std::path::PathBuf;
 
     #[test]
     fn test_load_contents() {
         let path = PathBuf::from(file!());
-        let contents = File::load_contents(&path);
+        let contents = File::load_contents(&path).unwrap();
 
         assert!(contents.contains("mod test_file_private {"));
     }
@@ -291,6 +330,31 @@ mod test_file_private {
         let path = PathBuf::from("/path/doesnt/exist");
         let contents = File::load_contents(&path);
 
-        assert_eq!(contents, String::new())
+        assert!(contents.is_err());
+        let err = contents.unwrap_err();
+
+        assert!(err.details.contains("No such file"));
+        assert!(format!("{}", err).contains("No such file"));
+
+        let err2 = err.clone();
+        assert_eq!(err.details, err2.details);
+    }
+
+    #[test]
+    fn test_runtime_creation() {
+        let rt = Runtime {
+            language: "clojure".to_string(),
+            version: "9000".to_string(),
+            aliases: vec![],
+        };
+
+        let rt2 = rt.clone();
+        assert_eq!(rt.language, rt2.language);
+        assert_eq!(rt.version, rt2.version);
+        assert_eq!(rt.aliases, rt2.aliases);
+
+        assert_eq!(rt.language, "clojure".to_string());
+        assert_eq!(rt.version, "9000".to_string());
+        assert!(rt.aliases.is_empty());
     }
 }
